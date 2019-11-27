fa496b13b5845e3d63b6806ffd20786ddd9d8455
Francisco Andrés Riquelme Cavagnola <francisco.riquelme@outlook.com>
2019-11-27 20:44:49 -0300

    Modified to load external Rdata and reports

--- app.R
+++ app.R
@@ -19,9 +19,22 @@ for (i in 1:length(packageVerification))
     pkgCheck(packageVerification[i])
 
 options(shiny.port = 5000)
+options(shiny.host  = '127.0.0.1')
 
 path <- getwd()
 path <- paste(path, "/core", sep = "")
 
-browseURL("http://127.0.0.1:5000/")
-runApp(appDir = path)
+if(length(ls(envir=.GlobalEnv, pattern="iraceResults")) == 0)
+  load('resources/data/iraceResults.Rdata', envir=.GlobalEnv)
+
+repeat{
+    browseURL("http://127.0.0.1:5000/")
+    returnData = runApp(appDir = path)
+    if(length(returnData) != 0)
+    {
+        rm(iraceResults, envir = .GlobalEnv)
+        load(returnData, envir=.GlobalEnv)
+        assign("loadedCustomSection", TRUE, envir=.GlobalEnv,inherits = FALSE)
+        print("Loaded file within app, restarting...")
+    }
+}
