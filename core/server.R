1bac74f246255e6b0e28c7d32222de4504373831
Francisco Andrés Riquelme Cavagnola <francisco.riquelme@outlook.com>
2019-12-21 17:18:54 -0300

    Stop on sessionEnded and list with data

--- core/server.R
+++ core/server.R
@@ -1,7 +1,12 @@
 server <- function(input, output, session) {
     observeEvent(input$reportLoader, {
         dataToLoad <- input$reportLoader
+        status <- list(goto = 2, path = dataToLoad$datapath)
 
-        stopApp(returnValue = invisible(dataToLoad$datapath))
+        stopApp(returnValue = invisible(status))
     }, once = TRUE)
+
+    session$onSessionEnded(function() {
+        stopApp()
+    })
 }
