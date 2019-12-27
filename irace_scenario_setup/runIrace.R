4915fead50b199de330204c1194ef478113926c6
Francisco Andrés Riquelme Cavagnola <francisco.riquelme@outlook.com>
2019-12-27 00:56:41 -0300

    Change to shared folder + remove plot

--- irace_scenario_setup/runIrace.R
+++ irace_scenario_setup/runIrace.R
@@ -1,7 +1,7 @@
 library('irace')
 
 print("Running IRACE...")
-setwd('../Irace_scenario-setup')
+setwd('../shared')
 parameters = readParameters(file="parameters.txt")
 scenario = readScenario(filename="scenario.txt")
 irace(scenario=scenario, parameters=parameters)
